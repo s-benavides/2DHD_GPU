b644aa7f33086c3925404a4a2124980d76e6f0d2
s-benavides <santiago.benavides@upm.es>
2025-11-17 15:40:09 +0100

    Added dec_dim parameter, used in fractal fourier decimation.

--- run_dir/run/parameter.py
+++ run_dir/run/parameter.py
@@ -13,7 +13,7 @@ Ti = np.int64
 ################
 ### Ensemble ###
 ################
-Nens = 10  # Number of ensemble members
+Nens = 40  # Number of ensemble members
 
 ##################
 ### Resolution ###
@@ -21,9 +21,11 @@ Nens = 10  # Number of ensemble members
 n = 256    # Resolution
 n_half = n//2+1
 kcut = Tf(n/3.0) # float
-ord = 4    # Runge-Kutta order
+ord = 2    # Runge-Kutta order
 kmax = Tf((kcut)**2)  #     kmax: maximum truncation for dealiasing
 tiny =  Tf(0.000001)   #     tiny: minimum truncation for dealiasing
+dec_dim = Tf(1.75)         # Fractional dimension for decimation (dec_dim = 2 does nothing)
+## NOTE: for now only works for iflow = 1!!
 
 ############
 ### Time ###
@@ -33,7 +35,7 @@ cfl_cad = 4           # Number of time steps before cfl is changed (saves some t
 H = np.inf            # Number of wall-hours to run for.
 step =  np.inf        # Numer of steps in run   
 cstep = 1000          # Number of steps between time series output
-thstep = 100          # Number of steps between theta histogram updates
+thstep = 200          # Number of steps between theta histogram updates
 thtsstep = Nens*1000  # Number of steps between theta time series output
 sstep = 500000        # Number of steps between spectra saves     
 tstep = 5000000       # Number of steps between field output
@@ -41,18 +43,18 @@ tstep = 5000000       # Number of steps between field output
 ########################
 ### Fluid parameters ###
 ########################
-fp0 = Tf(4.00)                # streamfunction forcing amplitude
-u0 = Tf(0.10)                  # streamfunction ic amplitude
-kdn = Tf(8.0)                # lowest forced wavenumber
-kup = Tf(9.0)                # highest forcing wavenumber
-nu = Tf(2e-6)         # viscosity, 0.001 for n=256 and nn=1
-hnu = Tf(0.5)   # hypoviscosity, 0.5 
-nn = Ti(2)                     # order of dissipation 
-mm = Ti(2)                     # order of hypo-dissipation
+fp0 = Tf(16.00)            # streamfunction forcing amplitude
+u0 = Tf(0.10)              # streamfunction ic amplitude
+kdn = Tf(35.0)             # lowest forced wavenumber
+kup = Tf(37.0)             # highest forcing wavenumber
+nu = Tf(8.e-29)            # viscosity, 0.001 for n=256 and nn=1
+hnu = Tf(0.7937)           # hypoviscosity, 0.5 
+nn = Ti(8)                 # order of dissipation 
+mm = Ti(2)                 # order of hypo-dissipation
 seed = 123456              # random seed
 iflow = 1                  # forcing choice (1 = sin(x)*sin(y), 2 = const inj, 3 = random forcing)
 dt_corr = 0.00             # forcing correlation time
-triad_phase_hist =False   # If true, then loads and updates histograms of triad phases
+triad_phase_hist = True   # If true, then loads and updates histograms of triad phases
 Nbins = 30                 # Sets the number of bins for the PDFs of thetas
 alpha = 1.75                # Initial KE spectrum (from largest scale to kup) is KE(k) = k**(-alpha), with integrated, total KE = u0. 
 beta = 1.75                 # Initial KE spectrum (from kup to smallest scales) is KE(k) = k**(-beta), with integrated, total KE = u0. ONLY USED IN PHASE ONLY VERSION
