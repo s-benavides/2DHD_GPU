7d48591c44a466ced3bca62f8f343bbc85e361f9
s-benavides <santiago.benavides@upm.es>
2026-06-16 16:54:21 +0200

    Added int32 for single precision, changed standard cfl cadance to 10.

--- run_dir/run/parameter.py
+++ run_dir/run/parameter.py
@@ -6,6 +6,7 @@ import cupy as np
 ###########################
 #Tf = np.float32
 #Tc = np.complex64
+#Ti = np.int32
 Tf = np.float64
 Tc = np.complex128
 Ti = np.int64
@@ -30,7 +31,7 @@ dec_dim = Tf(1.95)         # Fractional dimension for decimation (dec_dim = 2 do
 ### Time ###
 ############
 cfl = Tf(0.5) # CFL safety factor
-cfl_cad = 4           # Number of time steps before cfl is changed (saves some time).
+cfl_cad = 10           # Number of time steps before cfl is changed (saves some time).
 H = np.inf            # Number of wall-hours to run for.
 step =  np.inf        # Numer of steps in run   
 cstep = 1000          # Number of steps between time series output
